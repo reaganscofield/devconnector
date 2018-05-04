58f8c6582349461909536942815baef150029ab2
reaganscofield <reaganscofieldkayembe@outlook.com>
2018-05-04 17:04:58 +0200

    Feat: added post models and post validation and post post router to with rest api

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,39 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/teste', (request, response) => response.json({flashMesg: 'Posts Works'}));
+
+const Profile = require('../../models/profile');
+const Post = require('../../models/post');
+
+const validatePostInput = require('../../validation/post');
+
+
+router.get('/test', (request, response) => {
+    response.json({ msg: 'post works '});
+});
+
+router.get('/', (request, response) => {
+   Post.find()
+    .sort({ date: -1 }).then(posts => {
+        response.json(posts);
+    })
+    .catch(err => {
+        console.log(err);
+        response.status(404).json({ flashMsg: 'There is Not Post'});
+    })
+});
+
+router.get('/:id', (request, response) => {
+    Post.findById(request.params.id)
+        .then(post => {
+            response.json(post);
+            console.log(post);
+        })
+        .catch(err => {
+            console.log(err);
+            response.status(404).json({ flashMsg: 'Not Post Have Been Fund'});
+        })
+});
+
 
 module.exports = router;
---
+++ validation/post.js
@@ -0,0 +1,19 @@
+const Validator = require('validator');
+const isEmpty = require('./is-empty');
+
+module.exports = function validatePostInput(data) {
+    let errors = {};
+    data.text = !isEmpty(data.text) ? data.text : '';
+    if(!Validator.isLength(data.text, { min: 10, max: 300 })) {
+        errors.text = 'Post must be between 10 and 300 characaters';
+    }
+
+    if(Validator.isEmpty(data.text)) {
+        errors.text = 'Post Field is Required';
+    }
+
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    };
+};
